85238ca2c1d9b899c71f122cc8d9547663cf0fd0
ralpho55 <ralphjyammine@gmail.com>
2025-07-05 18:47:58 +0300

    Create function that simulates a round.
    The rules of rock paper scissors are introcuded in the function and depending on the results, prints if the human player won or lost or drew the round.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,17 @@
 function getComputerChoice(){
-    return Math.random() * 1 ;
+    let number = Math.random() * 1 ;
+    if(number <= 0.33)
+    {
+        return "rock";
+    }
+    else if (number > 0.33 && number <= 0.66)
+    {
+        return  "paper";
+    }
+    else
+    {
+        return "scissors";
+    }
 }
 
 function getHumanChoice(){
@@ -7,4 +19,31 @@ function getHumanChoice(){
     return choice;
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice){
+    let human = humanChoice.toLowerCase();
+    let computer = computerChoice.toLowerCase();
+
+    if(human == computer)
+    {
+        console.log("Draw");
+        return;
+    }
+
+    if((human == "rock" && computer == "scissors")
+    ||(human == "paper" && computer == "rock")
+    ||(human == "scissors" && computer == "paper"))
+    {
+
+        console.log(`You win! ${human} beats ${computer}`);
+
+    }
+    else{
+        console.log(`You lose! ${computer} beats ${human}`)
+    }
+    return;
+}
+
+let humanScore, computerScore = 0;
+
+//console.log(computerchoice);
+playRound(getHumanChoice(), getComputerChoice());
