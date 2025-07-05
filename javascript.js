6a9540ad2c082ab2431b044f39a4a36dd65ba050
ralpho55 <ralphjyammine@gmail.com>
2025-07-05 17:50:55 +0300

    Create functions that return both players choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+function getComputerChoice(){
+    return Math.random() * 1 ;
+}
+
+function getHumanChoice(){
+    let choice = prompt("Your turn: ")
+    return choice;
+}
+
+console.log(getHumanChoice());
